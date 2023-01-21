e96452b9bfba09d480ccc6c4f213b5cfb3e1aca2
Julian Barker <julian.c.barker@gmail.com>
2023-01-20 17:22:51 -0800

    feat: add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,51 @@
 import Products from './Components/Products'
 import Header from './Components/Header'
 import Categories from './Components/Categories';
+import {
+  createRouteConfig,
+  createReactRouter,
+  RouterProvider,
+  Outlet
+} from '@tanstack/react-router';
 
 import './App.css'
 
-function App() {
-  return (
+const rootRoute = createRouteConfig();
+
+const home = rootRoute.createRoute({
+  path: '/',
+  component: () => (
     <>
       <Header />
       <Categories />
       <Products />
+      <Outlet />
     </>
+  )
+});
+
+const cart = rootRoute.createRoute({
+  path: '/',
+  component: () => (
+    <>
+      <Header />
+      <Products />
+      <Outlet />
+    </>
+  )
+});
+
+const routeConfig = rootRoute.addChildren([home, cart]);
+
+const router = createReactRouter(routeConfig);
+
+function App() {
+  return (
+    <RouterProvider router={router} >
+    </RouterProvider>
   );
 }
+
+
 
 export default App
